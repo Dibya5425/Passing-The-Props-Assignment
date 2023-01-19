a85202e33becb7562d7dc0c353a9965c9d312e4e
dibya jyoti <dibya5425@gmail.com>
2023-01-19 20:29:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import Welcome from './Welcome';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name , setName] = useState("")
+    const [regNo , setregNo] = useState("")
+    const [submit , setSubmit] = useState(false)
+
+
+    return (
+
+        <div>
+        <div>
+            <label>Name</label>
+            <input
+            placeholder ="Enter Your Name"
+            value ={name}
+            onChange={(event)=>{setName(event.target.value)}}></input>
+             <label> Registration Number</label>
+            <input
+            placeholder ="Enter Your Registration Number"
+            value ={regNo}
+            onChange={(event)=>{setregNo(event.target.value)}}>
+
+            </input>
+            </div> 
+            <div><button onClick={()=>{setSubmit(!submit)}}>Submit</button> </div>
+
+            {(submit && name && regNo) && <Welcome name={name} regNo={regNo}></Welcome>}
+        
+
+        </div>
+
+        
+    );
+
 }
 
 export default App;
